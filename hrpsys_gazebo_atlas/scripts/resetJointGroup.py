6bfacbaa1c40634c3f4fca2a35f3adf9145c6cbf
Kei Okada <kei.okada@gmail.com>
2015-01-09 03:21:13 +0900

    Trimmed the branch to only the eusgazebo sub directory

--- hrpsys_gazebo_atlas/scripts/resetJointGroup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import roslib; roslib.load_manifest('hrpsys_gazebo_atlas')
-from hrpsys_gazebo_atlas.atlas_client import *
-
-if __name__ == '__main__':
-    shcf=ATLASHrpsysConfigurator()
-    shcf.init(sys.argv[1], sys.argv[2])
-
-    shcf.resetJointGroup()
