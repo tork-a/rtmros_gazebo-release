4272dafd3f14f1b627e8a7ad5063d1fcfa2c1b19
Kei Okada <kei.okada@gmail.com>
2015-01-09 03:20:54 +0900

    Trimmed the branch to only the hrpsys_gazebo_msgs sub directory

--- hrpsys_gazebo_atlas/scripts/atlas_user_mode.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-rostopic pub /atlas/atlas_sim_interface_command atlas_msgs/AtlasSimInterfaceCommand "{header: {stamp: now}, behavior: 1, k_effort: [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]}" -1
