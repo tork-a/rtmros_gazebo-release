45fc7bfffb5a5d7219ea94331b9946221111da3a
Kei Okada <kei.okada@gmail.com>
2015-01-08 21:21:15 +0900

    Revert "Trimmed the branch to only the hrpsys_gazebo_general sub directory"
    This reverts commit b625f17634f1d04a3c9d6ae2552b08f85caf82f5.

---
+++ hrpsys_gazebo_atlas/scripts/hrpsys_servo_on.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rosservice call /RobotHardwareServiceROSBridge/power '{name: "all", ss: 0}'
+sleep 1
+# $(rospack find hrpsys_gazebo_atlas)/scripts/resetJointGroup.py RobotHardware0 $(rospack find hrpsys_gazebo_atlas)/models/atlas_v3.dae -ORBInitRef NameService=corbaloc:iiop:localhost:15005/NameService
+sleep 1
+rosservice call /StateHolderServiceROSBridge/goActual
+sleep 1
+rosservice call /RobotHardwareServiceROSBridge/servo '{name: "all", ss: 0}'
