6e1ac6591e6bd86394276c5583a9b4e8d84eedb6
Kei Okada <kei.okada@gmail.com>
2016-03-18 13:45:36 +0900

    Trimmed the branch to only the hrpsys_moveit_configs/staro_moveit_config sub directory

--- hrpsys_gazebo_atlas/scripts/atlas_stand_mode.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-rostopic pub /atlas/atlas_sim_interface_command atlas_msgs/AtlasSimInterfaceCommand "{header: {stamp: now}, behavior: 0, k_effort: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}" -1
